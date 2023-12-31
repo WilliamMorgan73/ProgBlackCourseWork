9f4b46e667b052c339e373da705c2c2305cdc208
Will Morgan <sghp78@durham.ac.uk>
2023-12-31 15:37:07 +0000

    Updated java script

--- client/assests/js/doctor.js
+++ client/assests/js/doctor.js
@@ -1,40 +1,104 @@
-$(document).ready(function() {
-  $('.doctor-button').click(function(e) {
-      e.preventDefault();
+$(document).ready(function () {
+  $(".doctor-button").click(function (e) {
+    e.preventDefault();
 
-      // Get the doctorId from the clicked element's data-doctor-id attribute
-      const doctorId = $(this).data('doctor-id');
+    // Get the doctorId from the clicked element's data-doctor-id attribute
+    const doctorId = $(this).data("doctor-id");
 
-      // Make the AJAX request based on the obtained doctorId
-      $.get(`/doctor/${doctorId}`, function(data) {
-          // Update the HTML content based on the received JSON data
-          // Create a new unordered list for serials
-          var $ul = $('<ul/>');
+    // Make the AJAX request based on the obtained doctorId
+    $.get(`/doctor/${doctorId}`, function (data) {
+      // Update the HTML content based on the received JSON data
+      // Update the episodes section
+      if (data.episodes && data.episodes.length > 0) {
+        var $olEpisodes = $("<ul/>");
+        $.each(data.episodes, function (index, episode) {
+          $olEpisodes.append($("<li/>").text(episode));
+        });
+        $("#episodes").empty().append($olEpisodes);
+      } else {
+        $("#episodes").empty().text("No episodes available.");
+      }
 
-          // Loop through the stories (serials)
-          $.each(data.stories, function(index, story) {
-              // Create a new list item for each serial and append it to the unordered list
-              var $li = $('<li/>').text(story.serial);
+      // Update the companions section
+      if (data.companions && data.companions.length > 0) {
+        var $companionContainer = $('<div class="row"/>');
 
-              // Create a new ordered list for episodes within the serial
-              var $ol = $('<ol/>');
+        $.each(data.companions, function (index, companion) {
+          // Create a column for each companion
+          var $companionCol = $('<div class="col-md-4 mb-4"/>');
 
-              // Loop through the episodes
-              $.each(story.episodes, function(index, episode) {
-                  // Create a new list item for each episode and append it to the ordered list
-                  $ol.append($('<li/>').text(episode));
-              });
+          // Create an image element for each companion
+          var $companionImage = $(
+            '<img class="img-fluid rounded" alt="' + companion.name + '">'
+          );
+          $companionImage.attr(
+            "src",
+            "assests/images/companions/" + companion.id + ".png"
+          );
 
-              // Append the ordered list (episodes) to the serial's list item
-              $li.append($ol);
+          // Create a paragraph element for the companion's name
+          var $companionName = $(
+            '<p class="text-center font-weight-bold mb-0"/>'
+          ).text(companion.name);
 
-              // Append the serial's list item to the unordered list
-              $ul.append($li);
-          });
+          // Append image and name to the column
+          $companionCol.append($companionImage, $companionName);
 
-          // Set the text for the title and append the unordered list to the #episodes element
-          $('#episodes').empty().append('Episodes: ', $ul);
-          $('#companions').text('Companions: ' + data.companions.join(', '));
-      });
+          // Append the column to the container
+          $companionContainer.append($companionCol);
+        });
+
+        // Append the container to the companions section
+        $("#companions").empty().append($companionContainer);
+      } else {
+        $("#companions").empty().text("No companions available.");
+      }
+
+      // If there are monsters, update the monsters section
+      if (data.monsters && data.monsters.length > 0) {
+        var $monsterContainer = $('<div class="row"/>');
+
+        $.each(data.monsters, function (index, monster) {
+          // Ensure that monster is an object with an ID and name
+          if (
+            monster &&
+            typeof monster === "object" &&
+            monster.id &&
+            monster.name
+          ) {
+            // Create a column for each monster
+            var $monsterCol = $('<div class="col-md-4 mb-4"/>');
+
+            // Create an image element for each monster
+            var $monsterImage = $(
+              '<img class="img-fluid rounded" alt="' + monster.name + '">'
+            );
+            $monsterImage.attr(
+              "src",
+              "assests/images/monsters/" + monster.id + ".png"
+            );
+
+            // Create a paragraph element for the monster's name
+            var $monsterName = $(
+              '<p class="text-center font-weight-bold mb-0"/>'
+            ).text(monster.name);
+
+            // Append image and name to the column
+            $monsterCol.append($monsterImage, $monsterName);
+
+            // Append the column to the container
+            $monsterContainer.append($monsterCol);
+          }
+        });
+
+        // Append the container to the monsters section
+        $("#notable-monsters").empty().append($monsterContainer);
+      } else {
+        $("#notable-monsters").empty().text("No notable monsters available.");
+      }
+
+      // Make the relevant section visible
+      $(".invisible").removeClass("invisible");
+    });
   });
 });
